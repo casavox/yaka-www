49421a94523e7b21a791b09029c5b4bac0d12d1e
gonodv <vincent.gonod.epitech@gmail.com>
2016-01-21 19:31:52 +0100

    commit resolve socialNetwork

--- js/socialNetwork.js
+++ js/socialNetwork.js
@@ -14,9 +14,9 @@
         if (response.authResponse) {
           console.log('Welcome!  Fetching your information.... ');
           FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
+            console.log(response);
             var accessToken = FB.getAuthResponse().accessToken;
-            return accessToken;
+            resolve(accessToken);
           });
         } else {
           console.log('User cancelled login or did not fully authorize.');
