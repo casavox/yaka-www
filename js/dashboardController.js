1e3bed97f5b6a27106d3dd3915d596da8b9a3f47
gonodv <vincent.gonod.epitech@gmail.com>
2016-01-23 16:27:26 +0100

    modification limit dashboard projects

--- js/dashboardController.js
+++ js/dashboardController.js
@@ -29,7 +29,7 @@
       var tmp = [];
       if (tab.length > 0){
         for (var i = 0; i < tab.length; i++) {
-          if (tab[i] && tab[i].type && tab[i].status == "ONGOING_RATE_PRO")
+          if (tab[i] && tab[i].type && tab[i].status && tab[i].status == "ONGOING_RATE_PRO")
           tmp.push(tab[i]);
         }
         for (var i = 0; i < tab.length; i++) {
@@ -38,6 +38,11 @@
         }
         for (var i = 0; i < tab.length; i++) {
           if (tab[i] && tab[i].type && tab[i].unreadMessages == true && tab[i].type != "EMERGENCY" && tab[i].status != "ONGOING_RATE_PRO")
+          tmp.push(tab[i]);
+        }
+        if (tmp.length < 3)
+        for (var i = 0; i < tab.length; i++) {
+          if (tab[i] && tab[i].type && tab[i].status && tab[i].unreadMessages == false && tab[i].type != "EMERGENCY" && tab[i].status != "ONGOING_RATE_PRO")
           tmp.push(tab[i]);
         }
         return tmp;
