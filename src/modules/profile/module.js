a298a69adddd7ddaec01f9235ccd4093c8801a31
gonodv <vincent.gonod.epitech@gmail.com>
2016-05-06 01:58:58 +0200

    refactor(all) add files

--- src/modules/profile/module.js
+++ src/modules/profile/module.js
@@ -1,3 +1,27 @@
 /**
  * Created by Vincent on 30/04/2016.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('Yaka')
+        .config(config);
+
+    function config($stateProvider) {
+
+        $stateProvider
+            .state('profile', {
+                url: "/profile/me",
+                templateUrl: "modules/profile/views/profile.html",
+                controller: 'ProfileCustomerController',
+                controllerAs: 'vm'
+            })
+            .state('pro-profile', {
+                url: "/pro/profile/me",
+                templateUrl: "modules/profile/views/profile-pro.html",
+                controller: 'ProfileController',
+                controllerAs: 'vm'
+            });
+    }
+})();
