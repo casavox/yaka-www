7801f0184e2f634ace7a8e9f369bcbe2c48a8ece
Victor Leduc <contact@victorleduc.net>
2016-08-23 12:05:50 +0200

    Revert "Working on public recommand page"
    This reverts commit c244048d8f463f044ef1603ac96b24d9ac6026d4.

--- src/modules/project/controllers/public/public-project-recommend-controller.js
+++ src/modules/project/controllers/public/public-project-recommend-controller.js
@@ -5,7 +5,7 @@
         .module('Yaka')
         .controller('PublicProjectRecommendController', PublicProjectRecommendController);
 
-    function PublicProjectRecommendController($scope, $localStorage, $state, networkService, alertMsg, Upload, cloudinary, $filter, $stateParams, Lightbox, $rootScope, uiGmapGoogleMapApi, modalService, $translate, $auth) {
+    function PublicProjectRecommendController($scope, $localStorage, $state, networkService, alertMsg, Upload, cloudinary, $filter, $stateParams, Lightbox, $rootScope, uiGmapGoogleMapApi, modalService, $translate) {
 
         if (angular.isUndefined($stateParams.projectId) || !$stateParams.projectId) {
             $state.go("home");
@@ -160,153 +160,39 @@
             });
         };
 
+        vm.showInvitProPopup = false;
+
+        vm.openProPopup = function () {
+            vm.showInvitProPopup = true;
+        };
+
+        vm.closeProPopup = function () {
+            vm.showInvitProPopup = false;
+        };
+
         vm.recommendMsg = {
             text: ""
         };
-        /*
-         vm.recommendPro = function (proId) {
-         networkService.recommendProForProjectPOST(vm.project.id, proId, vm.recommendMsg, function (res) {
-         if ($localStorage.user && $localStorage.user.professional) {
-         $state.go('pro-dashboard');
-         } else {
-         $state.go('dashboard');
-         }
-         swal({
-         title: "C'est fait !",
-         text: "Vous avez bien recommandé ce professionel sur ce projet.",
-         type: "success",
-         showConfirmButton: true,
-         confirmButtonColor: "#03a9f4",
-         confirmButtonText: "Fermer"
-         });
-         }, function (err) {
-         alertMsg.send("Impossible de recommander ce professionnel", 'danger');
-         });
-         };*/
 
-        vm.newUser = {
-            password: "",
-            firstName: "",
-            lastName: "",
-            email: "",
-            googleId: "",
-            facebookId: "",
-            defaultAddress: {
-                address: ""
-            },
-            recaptchaResponse: "",
-            avatar: {
-                cloudinaryPublicId: ""
-            }
-        };
-
-        vm.autocomplete = {
-            options: {
-                types: ['(cities)'],
-                componentRestrictions: {country: 'fr'}
-            }
-        };
-
-        var doNotHide = false;
-
-        vm.needToHideEmail = function () {
-            if (doNotHide) {
-                return false;
-            }
-            if ((!angular.isUndefined(vm.newUser.googleId) && vm.newUser.googleId && vm.newUser.googleId != "") ||
-                (!angular.isUndefined(vm.newUser.facebookId) && vm.newUser.facebookId && vm.newUser.facebookId != "")) {
-                if (vm.newUser.email == '') {
-                    doNotHide = true;
-                    return false;
+        vm.recommendPro = function (proId) {
+            networkService.recommendProForProjectPOST(vm.project.id, proId, vm.recommendMsg, function (res) {
+                if ($localStorage.user && $localStorage.user.professional) {
+                    $state.go('pro-dashboard');
                 } else {
-                    return true;
+                    $state.go('dashboard');
                 }
-            }
-            return false;
-        };
-
-        vm.isSocialRegister = function () {
-            return !!((!angular.isUndefined(vm.newUser.googleId) && vm.newUser.googleId && vm.newUser.googleId != "") ||
-            (!angular.isUndefined(vm.newUser.facebookId) && vm.newUser.facebookId && vm.newUser.facebookId != ""));
-
-        };
-
-        vm.registerFormIsValid = function () {
-            return !(!vm.newUser.firstName || !vm.newUser.lastName || !vm.newUser.email ||
-            vm.newUser.password == '' || vm.newUser.password < 6 ||
-            vm.passwordConfirm == '' || vm.newUser.password != vm.passwordConfirm || vm.registering || !vm.newUser.defaultAddress.address || !vm.newUser.recaptchaResponse);
-        };
-
-
-        vm.googlePreRegister = function () {
-            vm.socialNetwork = "Google";
-            $auth.authenticate('googleRegister').then(function (res) {
-                if (!angular.isUndefined(res.data.googleId) && res.data.googleId && res.data.googleId != "") {
-                    onPreRegisterOK(res.data);
-                }
-            }).catch(function (res) {
-                if (res.data != undefined && res.data.error != undefined && res.data.error != "ERROR") {
-                    alertMsg.send($translate.instant(res.data.error), 'danger');
-                } else {
-                    alertMsg.send("Impossible de se connecter via Google", 'danger');
-                }
+                swal({
+                    title: "C'est fait !",
+                    text: "Vous avez bien recommandé ce professionel sur ce projet.",
+                    type: "success",
+                    showConfirmButton: true,
+                    confirmButtonColor: "#03a9f4",
+                    confirmButtonText: "Fermer"
+                });
+            }, function (err) {
+                alertMsg.send("Impossible de recommander ce professionnel", 'danger');
             });
         };
 
-        vm.facebookPreRegister = function () {
-            vm.socialNetwork = "Facebook";
-            $auth.authenticate('facebookRegister').then(function (res) {
-                if (!angular.isUndefined(res.data.facebookId) && res.data.facebookId && res.data.facebookId != "") {
-                    onPreRegisterOK(res.data);
-                }
-            }).catch(function (res) {
-                if (res.data != undefined && res.data.error != undefined && res.data.error != "ERROR") {
-                    alertMsg.send($translate.instant(res.data.error), 'danger');
-                } else {
-                    alertMsg.send("Impossible de se connecter via Facebook", 'danger');
-                }
-            });
-        };
-
-        function onPreRegisterOK(user) {
-            vm.newUser.firstName = user.firstName;
-            vm.newUser.lastName = user.lastName;
-            if (user.email != undefined) {
-                vm.newUser.email = user.email.toLowerCase();
-            }
-            vm.newUser.googleId = user.googleId;
-            vm.newUser.facebookId = user.facebookId;
-            vm.newUser.avatar = user.avatar;
-        }
-
-        vm.registering = false;
-
-        vm.register = function () {
-            if (vm.registerFormIsValid()) {
-                vm.registering = true;
-                networkService.register(vm.newUser, successRegister, failRegister);
-            }
-        };
-
-        function successRegister(res) {
-            vm.registering = false;
-            $localStorage.token = res.token;
-            $localStorage.user = res;
-            if ($localStorage.urlRedirect != undefined) {
-                window.location.href = $localStorage.urlRedirect;
-                delete $localStorage.urlRedirect;
-            } else {
-                $state.go('dashboard');
-            }
-        }
-
-        function failRegister(err) {
-            vm.registering = false;
-            if (err.error != undefined && err.error != "ERROR") {
-                alertMsg.send($translate.instant(err.error), 'danger');
-            } else {
-                alertMsg.send("Impossible de créer le compte", 'danger');
-            }
-        }
     }
 })();
