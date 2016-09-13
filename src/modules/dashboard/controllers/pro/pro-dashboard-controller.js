4f51784f2e239889dafeecb9da687c780ddb05c4
Victor Leduc <contact@victorleduc.net>
2016-09-13 15:19:31 +0200

    Debug

--- src/modules/dashboard/controllers/pro/pro-dashboard-controller.js
+++ src/modules/dashboard/controllers/pro/pro-dashboard-controller.js
@@ -81,17 +81,14 @@
             }
 
             if (project.id == '39a6cb12-17fd-423a-913b-386e03ea1d69') {
-                console.log("---------------");
+                console.log("----------------------------------------------------");
                 console.log("isInNetworkList");
                 console.log(project.id);
                 console.log("---------------");
             }
 
             for (var i = 0; i < vm.networkProjects.length; i++) {
-                if (project.id == '39a6cb12-17fd-423a-913b-386e03ea1d69') {
-                    console.log(vm.networkProjects[i].id);
-                    console.log("--");
-                }
+                console.log(vm.networkProjects[i].id);
                 if (project.id == vm.networkProjects[i].id) {
                     if (project.id == '39a6cb12-17fd-423a-913b-386e03ea1d69') {
                         console.log("OOOOOOKKKKKK");
