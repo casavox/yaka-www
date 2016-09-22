2dac8836f52e0044b59594099bc2dd1d266b2c32
Victor Leduc <contact@victorleduc.net>
2016-09-22 11:06:43 +0200

    Fix crash

--- src/modules/core/modules/api/network-service.js
+++ src/modules/core/modules/api/network-service.js
@@ -222,6 +222,9 @@
             projectPUT: function (data, success, error, ignoreLoading) {
                 httpService.put(baseUrl + '/projects/' + data.id, data, ignoreLoading).success(success).error(error);
             },
+            me: function (success, error, ignoreLoading) {
+                httpService.get(baseUrl + '/me', ignoreLoading).success(success).error(error)
+            },
             proposalPUT: function (data, success, error, ignoreLoading) {
                 httpService.put(baseUrl + '/pro/proposals/' + data.id, data, ignoreLoading).success(success).error(error);
             },
