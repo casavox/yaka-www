418b65ba1ac84f8813915cede24dba64e822d57c
Gregory Bizet <yaka4app+stage@gmail.com>
2016-10-14 09:41:25 +0200

    communities tags pro side ok

--- src/modules/proposal/controllers/pro/job-pro-list-controller.js
+++ src/modules/proposal/controllers/pro/job-pro-list-controller.js
@@ -52,5 +52,24 @@
                     return 'dès que possible';
             }
         };
+
+        vm.communities = [
+            {
+                "type": "ville",
+                "text": "Paris - 13ème arrondissement"
+            },
+            {
+                "type": "entreprise",
+                "text": "Okabé"
+            },
+            {
+                "type": "cercle",
+                "text": "Famille"
+            },
+            {
+                "type": "member",
+                "text": "Prenium"
+            }
+        ];
     }
 })();
