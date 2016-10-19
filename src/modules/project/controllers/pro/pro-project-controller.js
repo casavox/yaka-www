035cd50fcc3702df90ddd716f77b1308a5def597
Gregory Bizet <yaka4app+stage@gmail.com>
2016-10-19 16:02:53 +0200

    tags for material in project ok

--- src/modules/project/controllers/pro/pro-project-controller.js
+++ src/modules/project/controllers/pro/pro-project-controller.js
@@ -262,6 +262,8 @@
                 }
                 if (vm.project.hasMaterial) {
                     res.push("MATERIAL_TRUE");
+                } else {
+                    res.push("MATERIAL_FALSE");
                 }
             }
             return res;
